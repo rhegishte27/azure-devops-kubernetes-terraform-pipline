9ed1f5d1f83b6a1c6e5cf092343e54039bebaee1
rhegishte <rhegishte27@gmail.com>
2020-09-05 13:04:22 +0530

    Update Subnets 5 Sep 2020

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -32,7 +32,7 @@ resource "aws_eks_cluster" "aws_eks" {
   role_arn = aws_iam_role.eks_cluster.arn
 
   vpc_config {
-    subnet_ids = ["subnet-099f6465e0c5a4294", "subnet-02a5d1fb27edea60e"]
+    subnet_ids = ["subnet-0e1ef4158e266915b", "subnet-03cb7f8113c53bf1a"]
   }
 
   tags = {
@@ -78,7 +78,7 @@ resource "aws_eks_node_group" "node" {
   cluster_name    = aws_eks_cluster.aws_eks.name
   node_group_name = "node_tuto"
   node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = ["subnet-099f6465e0c5a4294", "subnet-02a5d1fb27edea60e"]
+  subnet_ids      = ["subnet-0e1ef4158e266915b", "subnet-03cb7f8113c53bf1a"]
 
   scaling_config {
     desired_size = 1
