e104b3e3a1eb2412cd188fe5bf0d7f1373cc514a
rhegishte27 <35556401+rhegishte27@users.noreply.github.com>
2023-03-30 21:42:40 +0530

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -14,12 +14,7 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   resource_group_name = azurerm_resource_group.resource_group.name
   dns_prefix          = var.dns_prefix
 
-  linux_profile {
-    admin_username = "ubuntu"
 
-    ssh_key {
-      key_data = file(var.ssh_public_key)
-    }
   }
 
   default_node_pool {
