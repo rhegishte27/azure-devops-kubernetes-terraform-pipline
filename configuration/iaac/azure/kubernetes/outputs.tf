dbbe36f83405b2120bfb562da175e00410af6673
rhegishte27 <35556401+rhegishte27@users.noreply.github.com>
2023-03-30 21:48:41 +0530

    Update outputs.tf

--- configuration/iaac/azure/kubernetes/outputs.tf
+++ configuration/iaac/azure/kubernetes/outputs.tf
@@ -1,27 +1,33 @@
-output "client_key" {
-  value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.client_key
-}
+# output "client_key" {
+#   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.client_key
+# }
 
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.client_certificate
-}
+# output "client_certificate" {
+#   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.client_certificate
+# }
 
-output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.cluster_ca_certificate
-}
+# output "cluster_ca_certificate" {
+#   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.cluster_ca_certificate
+# }
 
-output "cluster_username" {
-  value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.username
-}
+# output "cluster_username" {
+#   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.username
+# }
 
-output "cluster_password" {
-  value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.password
-}
+# output "cluster_password" {
+#   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.password
+# }
+
+# output "kube_config" {
+#   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config_raw
+# }
+
+# output "host" {
+#   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.host
+# }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.terraform-k8s.kube_config_raw
-}
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
 
-output "host" {
-  value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.host
+  sensitive = true
 }
